7107286ad0780f306437fd50850eebb25ebf0ea2
qvchoq <xakepq@gmail.com>
2021-09-14 23:52:18 +0300

    *[lesson2.task1]* Completed functions: 1) ageDescription (Easy) 2) whichRookThreatens (Easy) Coming soon: 1) segmentLength (Middle)

--- src/lesson2/task1/IfElse.kt
+++ src/lesson2/task1/IfElse.kt
@@ -68,7 +68,13 @@ fun minBiRoot(a: Double, b: Double, c: Double): Double {
  * Мой возраст. Для заданного 0 < n < 200, рассматриваемого как возраст человека,
  * вернуть строку вида: «21 год», «32 года», «12 лет».
  */
-fun ageDescription(age: Int): String = TODO()
+fun ageDescription(age: Int): String = when {
+    age % 100 % 10 in 5..20 -> "$age лет"
+    age % 100 in 5..20 -> "$age лет"
+    age % 10 == 1 -> "$age год"
+    age % 100 % 10 in 2..4 -> "$age года"
+    else -> "Error"
+}
 
 /**
  * Простая (2 балла)
@@ -96,7 +102,13 @@ fun whichRookThreatens(
     kingX: Int, kingY: Int,
     rookX1: Int, rookY1: Int,
     rookX2: Int, rookY2: Int
-): Int = TODO()
+): Int = when {
+    ((kingX == rookX1 || kingY == rookY1) && (kingX != rookX2 && kingY != rookY2)) -> 1
+    ((kingX == rookX2 || kingY == rookY2) && (kingX != rookX1 && kingY != rookY1)) -> 2
+    ((kingX == rookX2 || kingY == rookY2) && (kingX == rookX1 || kingY == rookY1)) -> 3
+    //((kingX == rookX1) && (kingY == rookY2)) || ((kingY == rookY1) && (kingX == rookX2)) || ((kingX == rookX1) && (kingY == rookY1)) || ((kingX == rookX2) && (kingY == rookY2)) -> 3
+    else -> 0
+}
 
 /**
  * Простая (2 балла)
