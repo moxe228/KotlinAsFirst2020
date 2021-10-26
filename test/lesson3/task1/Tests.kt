66bdc65e106fed6d05378cdb230b891e33c43ff5
qvchoq <xakepq@gmail.com>
2021-10-26 20:03:52 +0300

    lesson4.task1

--- test/lesson3/task1/Tests.kt
+++ test/lesson3/task1/Tests.kt
@@ -223,6 +223,7 @@ class Tests {
         assertEquals(6, squareSequenceDigit(12))
         assertEquals(0, squareSequenceDigit(17))
         assertEquals(9, squareSequenceDigit(27))
+        assertEquals(6, squareSequenceDigit(401179))
     }
 
     @Test
--- test/lesson4/task1/Tests.kt
+++ test/lesson4/task1/Tests.kt
@@ -100,6 +100,7 @@ class Tests {
         assertEquals(1.0, mean(listOf(1.0)), 1e-5)
         assertEquals(2.0, mean(listOf(3.0, 1.0, 2.0)), 1e-5)
         assertEquals(3.0, mean(listOf(0.0, 2.0, 7.0, 8.0, -2.0)), 1e-5)
+        assertEquals(-74797986907937570000.0, mean(listOf(-74797986907937570000.0)))
     }
 
     @Test
@@ -120,6 +121,10 @@ class Tests {
             listOf(-3.0, -1.0, 4.0, 5.0, -5.0).toDoubleArray(),
             center(mutableListOf(0.0, 2.0, 7.0, 8.0, -2.0)).toDoubleArray(),
             1e-5
+        )
+        assertArrayEquals(
+            listOf(-74797986907937570000.0).toDoubleArray(),
+            center(mutableListOf(-74797986907937570000.0)).toDoubleArray()
         )
         val toMutate = mutableListOf(-3.0, -1.0, 4.0, 5.0, -5.0)
         assertTrue(toMutate === center(toMutate)) { "You should mutate an input list, not create a copy" }
